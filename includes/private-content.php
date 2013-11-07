b3d321e4fff73b6dc604f99c8b5c114b14757cad
thomstark <thomstark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-07 22:19:52 +0000

    uploading version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/file-away/trunk@800710 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/private-content.php
@@ -0,0 +1,29 @@
+<?php
+
+$private_content = false;
+
+// If 'fa-userid' is entered anywhere in the sub attribute, it will be replaced with the current user's user id.
+if ( stripos ( $dir, 'fa-userid' ) ) {
+	$private_content = true;
+	$dir = str_ireplace ( 'fa-userid', $fa_userid, $dir );
+}
+
+// If 'fa-userrole' is entered anywhere in the sub attribute, it will be replaced with the current user's userrole.
+if ( stripos ( $dir, 'fa-userrole' ) ) { 
+	$private_content = true;
+	$dir = str_ireplace ( 'fa-userrole', strtolower ( $fa_userrole ), $dir );
+}
+
+// If 'fa-username' is entered anywhere in the sub attribute, it will be replaced with the current user's username.
+if ( stripos ( $dir, 'fa-username' ) ) { 
+	$private_content = true;
+	$dir = str_ireplace ( 'fa-username', strtolower ( $fa_username ), $dir );
+}
+	
+// If 'fa-firstlast' is found anywhere in the base or sub attributes, it will be replaced with the current user's firstname and lastname.
+if ( stripos ( $dir, 'fa-firstlast' ) ) { 
+	$private_content = true;
+	$dir = str_ireplace ( "fa-firstlast", strtolower ( $fa_firstlast ), $dir );
+}
+
+?>
