0de3a90d978a870ad13d17b88cf68bdb658a17da
thomstark <thomstark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-12 16:10:38 +0000

    uploading 1.4 to tags
    git-svn-id: https://plugins.svn.wordpress.org/file-away/tags/1.4@874120 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/file-type-icons.php
@@ -0,0 +1,56 @@
+<?php
+strtolower ( $ext );
+$linktype = 'download';
+if ( SSFA_NEWWINDOW !== '' ) {
+	$newwindows = preg_split( "/(,\s|,)/", preg_replace( '/\s+/', ' ', SSFA_NEWWINDOW ) );
+	foreach ( $newwindows as $new ) { if ( $ext === $new || '.'.$ext === $new ) { $linktype = 'target="_blank"'; break; } } }
+if ( !$icons || $icons === 'filetype' ) {
+	$iconmatch = false;
+	if ( $iconmatch === false ) {
+		$adobe = array('pdf', 'ai', 'aep', 'eps', 'flv', 'fla', 'psd', 'indd', 'pmd', 'fm', 'afm', 'abf', 'psb', 'pdd', 'prc', 'as', 'ppj', 'swf', 'ps');
+		foreach ( $adobe as $a ) { if ( $ext === $a ) { $icon = '&#x21;'; $iconmatch = true; break; } } }
+	if ( $iconmatch === false ) {
+		$pics = array('jpeg', 'jfif', 'jpg', 'gif', 'bmp', 'png', 'tiff', 'tif', 'raw', 'ppm' , 'exif', 'pgm', 'pbm', 'pnm', 'pfm', 'pam', 'webp', 'hdr', 'rgbe', 'iff', 'tga', 'jxr', 'hdp', 'wdp', 'dds', 'thm', 'yuv');
+		foreach ( $pics as $a ) { if ( $ext === $a ) { $icon = '&#x31;'; $iconmatch = true; break; } } }
+	if ( $iconmatch === false ) {
+		$zips = array('zip', '7z', 'rar', 'gz', 'a', 'ar', 'cpio', 'shar', 'tar', 'mar', 'bz2', 'lz', 'lzma', 'lzo', 'rz', 'sfark', 'xz', 'z', 's7z', 'ace', 'afa', 'cab', 'cfs', 'cpt', 'dar', 'dd', 'dmg', 'sda', 'tar.gz', 'tgz', 'zipx', 'zz');
+		foreach ( $zips as $a ) { if ( $ext === $a ) { $icon = '&#x27;'; $iconmatch = true; break; } } }
+	if ( $iconmatch === false ) {
+		$docs = array('doc', 'docx', 'dot', 'dotx'); 
+		foreach ( $docs as $a )	{ if ( $ext === $a ) { $icon = '&#x23;'; $iconmatch = true; break; } } }
+	if ( $iconmatch === false ) {
+		$excel = array('xls', 'xlsx', 'xlw', 'xlt', 'xlsm', 'xltx', 'xltm', 'xlsb');
+		foreach ( $excel as $a ) { if ( $ext === $a ) { $icon = '&#x24;'; $iconmatch = true; break; } } }
+	if ( $iconmatch === false ) {
+		$openoffice = array('odp', 'ods', 'odt', 'dbf', 'sxw', 'stw', 'sxc', 'stc', 'sxi', 'sti');
+		foreach ( $openoffice as $a ) { if ( $ext === $a ) { $icon = '&#x22;'; $iconmatch = true; break; } } }
+	if ( $iconmatch === false ) {
+		$texts = array('wpd', 'wps', 'xml', 'rtf', 'txt', 'log', 'csv', 'uot', 'uof', 'psw', 'wk1', 'wks', '123', 'sql');
+		foreach ( $texts as $a ) { if ( $ext === $a ) { $icon = '&#x2e;'; $iconmatch = true; break; } } }
+	if ( $iconmatch === false ) {
+		$music = array('wav', 'mp3', 'ram', 'aac', 'amr', 'm4a', 'mp2', 'mid', 'm4b', 'ogg', 'aif', 'aiff');
+		foreach ( $music as $a ) { if ( $ext === $a ) { $icon = '&#x43;'; $iconmatch = true; break; } } }
+	if ( $iconmatch === false ) {
+		$videos = array('wmv', 'qt', 'avi', 'mkv', 'mp4', 'm4v', 'rmvb', 'vob', 'rm', 'divx', 'mov', 'm4p', 'mpeg', 'mpg');
+		foreach ( $videos as $a ) { if ( $ext === $a ) { $icon = '&#x57;'; $iconmatch = true; break; } } }
+	if ( $iconmatch === false ) {
+		$powerpoint = array('pps', 'ppt', 'pot', 'pptx', 'pptm', 'potx', 'potm', 'pub');
+		foreach ( $powerpoint as $a ) { if ( $ext === $a ) { $icon = '&#x26;'; $iconmatch = true; break; } } }
+	if ( $iconmatch === false ) { 
+		$apps = array('bat', 'dll', 'exe', 'msi'); 
+		foreach ( $apps as $a ) { if ( $ext === $a ) { $icon = '&#x54;'; $iconmatch = true; break; } } }		
+	if ( $iconmatch === false ) {
+		$scripts = array('js', 'pl', 'py', 'rb', 'php', 'htm', 'html', 'cgi', 'asp', 'cfm', 'cpp', 'yml', 'shtm', 'shtml', 'xhtm', 'xhtml', 'java', 'clas', 'class');
+		foreach ( $scripts as $a ) { if ( $ext === $a ) {$icon = '&#x25;'; $iconmatch = true; break; } } }
+	if ( $iconmatch === false ) {
+		$css = array('css', 'less', 'scss', 'sass');
+		foreach ( $css as $a ) { if ( $ext === $a ) { $icon = '&#x28;'; $iconmatch = true; break; } } }
+	$icon = ( $iconmatch === false ? '&#x29;' : $icon );
+	$iconstyle = ( $type === 'table' ? 'ssfa-faminicon' : 'ssfa-listicon' );
+	$icon = "<span data-ssfa-icon='$icon' class='$iconstyle $icocol' aria-hidden='true'></span>";
+	$icon = ( $type === 'table' ? $icon.'<br />' : $icon ); } 
+else {
+	$papersize = ( $type === 'table' ? ' style="font-size:18px;"' : null );
+	$icon = ( $icons === 'paperclip' ? "<span data-ssfa-icon='&#xe1d0;' class='ssfa-paperclip $icocol' $papersize aria-hidden='true'></span>" : null );
+	$icon = ( $type === 'table' ? $icon.'<br />' : $icon ); }
+?>
