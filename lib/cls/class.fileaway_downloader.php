f5dd2fdec623ab82cfe9c272390c0f93759480d7
thomstark <thomstark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-09 23:43:09 +0000

    2.8.1 to tags
    git-svn-id: https://plugins.svn.wordpress.org/file-away/tags/2.8.1@985866 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/cls/class.fileaway_downloader.php
@@ -0,0 +1,63 @@
+<?php
+isset($_REQUEST['fileaway']) or die('Water, water everywhere, but not a drop to drink.');
+$parse_uri = explode('wp-content', $_SERVER['SCRIPT_FILENAME']);
+require_once($parse_uri[0].'wp-load.php');
+if(!class_exists('fileaway_downloader'))
+{
+	class fileaway_downloader
+	{
+		public function __construct()
+		{
+			$this->download();	
+		}
+		public function download()
+		{
+			$file = $this->decrypt($_GET['fileaway']);
+			$file = str_replace('%26', '&', str_replace('%3F', '?', $file));
+			if(!is_file($file)) die('File does not exist.');
+			$name = basename($file);
+			$size = filesize($file);
+			header("Pragma: public");
+			header("Expires: 0");
+			header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+			header("Cache-Control: public");
+			header("Content-Description: File Transfer");
+			header("Content-Type: application/force-download");
+			header("Content-Disposition: attachment; filename=\"$name\"");
+			header("Content-Transfer-Encoding: binary");
+			header("Content-Length: " . $size);
+			$file = @fopen($file, 'rb');
+			if($file)
+			{
+				while(!feof($file))
+			  {
+					print(fread($file, 1024*8));
+					flush();
+					if(connection_status()!=0)
+					{
+						@fclose($file);
+						die();
+					}
+				}
+				@fclose($file);
+			}
+			die();			
+		}
+		private function decrypt($file)
+		{
+			$result = '';
+			$op = get_option('fileaway_options');
+			$key = $op['encryption_key'];
+			$file = urldecode($file);
+			for($i = 1; $i <= strlen($file); $i++)
+			{
+				$char = substr($file, $i-1, 1);
+				$keychar = substr($key, ($i % strlen($key))-1, 1);
+				$char = chr(ord($char)-ord($keychar));
+				$result .= $char;
+			}
+			return $result;
+		}				
+	}	
+}
+new fileaway_downloader;
